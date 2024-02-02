3dde158ba6fc67ea0f02123588447961464cf08c
Lucas Montezuma <lucas.montezumaa@gmail.com>
2024-02-02 16:36:53 -0300

    first commit

---
+++ teste.js
@@ -0,0 +1,5 @@
+"use strict";
+function multiplicacao(num1, num2) { return num1 * num2; }
+const saudacao = (nome) => "Olá" + " " + nome;
+console.log(saudacao("Lucas"));
+console.log(multiplicacao(7, 3));
---
+++ teste.ts
@@ -0,0 +1,6 @@
+function multiplicacao(num1: number,num2: number): number { return num1 * num2}
+const saudacao = (nome:string):string => "Olá" + " " + nome;
+
+console.log(saudacao("Lucas")) 
+
+console.log(multiplicacao(7,3))
